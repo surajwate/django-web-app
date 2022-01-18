4c975703e2d1e1b18b758be0de1588fe4ca84897
Suraj Wate <suraj.wate@gmail.com>
2022-01-18 07:54:26 +0000

    used if condition in template

--- dproject/blog/views.py
+++ dproject/blog/views.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.shortcuts import render
 
 posts = [
@@ -22,4 +23,4 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
