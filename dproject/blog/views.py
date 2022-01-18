e8d60e8a9163a54d88b90aa7ad2d1fdabc11cbf0
Suraj Wate <suraj.wate@gmail.com>
2022-01-18 07:48:31 +0000

    Used for loop to display content

--- dproject/blog/views.py
+++ dproject/blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Suraj Wate',
+        'title': 'Blog Post 1',
+        'content': 'Content of the first blog post.',
+        'date_posted': 'January 17, 2022'
+    },
+    {
+        'author': 'Kushal Wate',
+        'title': 'Blog Post 2',
+        'content': 'Content of the second blog post.',
+        'date_posted': 'January 18, 2022'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
