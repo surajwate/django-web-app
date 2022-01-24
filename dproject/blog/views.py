40ab923ed1291907aed7f1c73097c992a015fb41
Suraj Wate <suraj.wate@gmail.com>
2022-01-24 16:59:59 +0000

    updated database and changed the view

--- dproject/blog/views.py
+++ dproject/blog/views.py
@@ -1,26 +1,14 @@
 from turtle import title
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Suraj Wate',
-        'title': 'Blog Post 1',
-        'content': 'Content of the first blog post.',
-        'date_posted': 'January 17, 2022'
-    },
-    {
-        'author': 'Kushal Wate',
-        'title': 'Blog Post 2',
-        'content': 'Content of the second blog post.',
-        'date_posted': 'January 18, 2022'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
